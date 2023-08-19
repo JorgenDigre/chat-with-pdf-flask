dd6275bf806284bde9f97167b86f6c49ec90c3ff
JorgenDigre <jorgen.digree@gmail.com>
2023-08-18 18:10:39 -0700

    inital commit

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, jsonify, render_template, request
+from dotenv import load_dotenv
+import os 
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+
+app = Flask(__name__)
+
+load_dotenv()
+
+os.getenv("OPENAI_API_KEY")
+
+knowledge_base = None
+chain = None
+messages = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global knowledge_base, chain
+    
+    if request.method == "POST":
+        api_key = request.form["api-key"]
+        
+        if api_key:
+            os.environ["OPENAI_API_KEY"] = api_key
+            
+            pdf = "/Users/jorgendigre/Desktop/tariff2.pdf"
+            
+            if pdf is not None:
+                pdf_reader = PdfReader(pdf)
+                text = ""
+                for page in pdf_reader.pages:
+                    text += page.extract_text()
+                    
+                
+                
+                text_splitter = CharacterTextSplitter(
+                    separator="\n",
+                    chunk_size=1000,
+                    chunk_overlap=200,
+                    length_function=len
+                )
+                
+                chunks = text_splitter.split_text(text=text)
+        
+                embeddings = OpenAIEmbeddings()
+                knowledge_base = FAISS.from_texts(chunks, embeddings)
+                
+                chain = load_qa_chain(OpenAI(), chain_type="stuff")
+        
+    return render_template("index.html", messages=messages)
+
+@app.route('/ask', methods=['POST'])
+def ask_question():
+    global messages
+
+    if request.method == 'POST':
+        user_question = request.form.get('user-question')
+
+        if knowledge_base is not None and chain is not None:
+            # Perform question-answering here
+            docs = knowledge_base.similarity_search(user_question)
+            result = chain.run(input_documents=docs, question=user_question)
+            
+
+            # Check if result is a dictionary with 'answers' key
+            
+
+            # Add user's question and response to messages
+            messages.append(f'You: {user_question}')
+            messages.append(f'AI: {result}')
+
+    return render_template("index.html", messages=messages)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
